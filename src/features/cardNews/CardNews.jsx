4bec973d3534b9339f619fd53a669b25f6d4429a
Amange1di <amangeldi2702006@gmail.com>
2025-03-11 17:27:09 +0300

    ваыуцууц

--- src/features/cardNews/CardNews.jsx
+++ src/features/cardNews/CardNews.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./CardNews.scss";
+import "./сardNews.scss";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Scrollbar } from "swiper/modules";
